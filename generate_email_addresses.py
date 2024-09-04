5a6124b012f0cb4be78c61dc74aa822aec480f1a
reinhard <buyabor@gmail.com>
2024-09-04 09:33:38 +0300

    This is the first commit

---
+++ generate_email_addresses.py
@@ -0,0 +1,4 @@
+# Define Module - Using docstring
+"""This python module generates email addresses of all students based on the specifications of..."""
+"""No special addresses in the file"""
+""""""
