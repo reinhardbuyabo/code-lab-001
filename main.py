f50cbadb2c1f43d2004972b43c8b8b6eccfd658a
Wendy-Lagho <wendy.lagho@strathmore.edu>
2024-09-04 21:16:24 +0300

    generate emails

--- main.py
+++ main.py
@@ -4,13 +4,29 @@
     # Press the green button in the gutter to run the script.
 # if __name__ == '__main__':
 #     print_hi('PyCharm')
-from functions import read_excel_file
 
+from functions import read_excel_file
+from generate_email_addresses import generate_unique_emails
 file_path = "Test Files.xlsx"
 
 df = read_excel_file(file_path)
 
 if df is not None:
+    print("Original DataFrame:")
     print(df.head())
+
+    unique_emails = generate_unique_emails(df)
+
+    # Add emails to DataFrame
+    df['Email Address'] = unique_emails
+
+    # updated DataFrame
+    print("\nUpdated DataFrame with email addresses:")
+    print(df.head())
+
+    print("\nStudent Names and Email Addresses:")
+    for _, row in df.iterrows():
+        print(f"Student Name: {row['Student Name']} - Email address: {row['Email Address']}")
+
 else:
     print("Failed to read the Excel file.")
