2b2f330de79ae688ee2c3c3360a9123095f174e7
reinhard <buyabor@gmail.com>
2024-09-04 09:48:35 +0300

    Getting Started

--- generate_email_addresses.py
+++ generate_email_addresses.py
@@ -1,4 +1,5 @@
-# Define Module - Using docstring
-"""This python module generates email addresses of all students based on the specifications of..."""
-"""No special addresses in the file"""
-""""""
+"""This python module generates email addresses of all students based on the specifications of...
+- all addresses should be unique
+- use two names if available
+- no special characters in the emails
+"""
