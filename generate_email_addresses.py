bbdbf329b74167a9c9b2a85351826079ad7ef60a
reinhard <buyabor@gmail.com>
2024-09-04 09:56:27 +0300

    TODO: Task 1

--- generate_email_addresses.py
+++
@@ -1,5 +0,0 @@
-"""This python module generates email addresses of all students based on the specifications of...
-- all addresses should be unique
-- use two names if available
-- no special characters in the emails
-"""
