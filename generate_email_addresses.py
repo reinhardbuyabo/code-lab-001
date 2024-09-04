0c64672d15c750883985f65002e9f7914089e9c5
reinhard <buyabor@gmail.com>
2024-09-04 09:56:27 +0300

    TODO: Task 1

--- generate_email_addresses.py
+++
@@ -1,4 +0,0 @@
-# Define Module - Using docstring
-"""This python module generates email addresses of all students based on the specifications of..."""
-"""No special addresses in the file"""
-""""""
