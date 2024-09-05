d32d1e8b855a662ca5a0a1353d2c47a5f769defc
reinhard <buyabor@gmail.com>
2024-09-05 18:35:55 +0300

    Thursday Review

--- generate_email_addresses.py
+++ generate_email_addresses.py
@@ -1,9 +1,4 @@
-"""This python module generates email addresses of all students based on the specifications of...
-- all addresses should be unique
-- use two names if available
-- no special characters in the emails
-"""
-
+# importing regular expressions
 import re
 
 def generate_email(name):
